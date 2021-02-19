c2ab82d1615c4ede2d9ec7a1b5ba93969fc60c76
Samuel Fox <samueljasonfox@gmail.com>
2021-02-18 23:44:14 -0800

    added npm packages, added link and script tags, added TestModal component with small example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import Loading from "./components/Loading"
+import TestModal from "./components/TestModal"
+import { Modal, Button } from 'react-materialize';
+
 
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App center-align">
+    <Loading />
+    <TestModal />
+  </div>;
 }
 
 export default App;
