c52a6181118c6555c85d30517d4ad443bafc23a7
Samuel Fox <samueljasonfox@gmail.com>
2021-02-23 00:00:31 -0800

    refactor timer component

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,17 +1,12 @@
-import React, { useState, useEffect, useRef } from 'react';
-
-// example used from https://dev.to/abdulbasit313/how-to-develop-a-stopwatch-in-react-js-with-custom-hook-561b
 export default function Timer({ timer }) {
-
-    // const [timer, setTimer] = useState(0)
-    // const increment = useRef(null)
-
 
 
     const formatTime = () => {
+
         const getSeconds = `0${(timer % 60)}`.slice(-2)
         const minutes = `${Math.floor(timer / 60)}`
         const getMinutes = `0${minutes % 60}`.slice(-2)
+
         return `${getMinutes} : ${getSeconds}`
     }
 
@@ -19,3 +14,5 @@ export default function Timer({ timer }) {
         formatTime()
     );
 }
+
+// example used from https://dev.to/abdulbasit313/how-to-develop-a-stopwatch-in-react-js-with-custom-hook-561b
