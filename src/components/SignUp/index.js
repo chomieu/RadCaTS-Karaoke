c28e6bf0f1c9222ccad7044c7b599b2fa3ce7b1f
Tim Keller <timothy.m.keller@gmail.com>
2021-02-23 14:24:40 -0800

    Added basic modals for sign in and sign up.

---
+++ src/components/SignUp/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./style.css";
+import { Modal, Button } from 'react-materialize';
+
+function SignIn() {
+    return (
+        <Modal>
+            Schnarvle The Barvle!!
+        </Modal>
+    )
+}
+
+export default SignIn;
