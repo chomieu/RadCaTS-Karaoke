c5c24e070c8dcf303231b100be9e07e90c56ffca
Samuel Fox <samueljasonfox@gmail.com>
2021-02-25 04:17:35 -0800

    search component added with title, form, and submit button

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,40 +1,54 @@
 import React, { useState } from 'react'
-import { Container, Row, Col, TextInput } from 'react-materialize';
+import { Container, Row, Col, TextInput, Button, Icon } from 'react-materialize';
+import "./style.css"
 
-function search() {
+function Search() {
 
     const [formInputs, setFormInputs] = useState({ artist: '', song: '' })
 
     const handleFormInputs = e => {
-        // const { name, value } = e.target
-        // setFormInputs({ ...formInputs, [name]: value })
+        const { name, value } = e.target
+        setFormInputs({ ...formInputs, [name]: value })
+    }
+
+    const handleSearch = e => {
+        e.preventDefault()
+
     }
 
     return (
-        <Container>
-            <Row>
-                <Col s={12}>
-                    <form >
-                        <TextInput
-                            icon="email"
-                            label="artist"
-                            name="artist"
-                        // value={formInputs.artist}
-                        // onChange={handleFormInputs}
-                        />
-                        <TextInput
-                            icon="email"
-                            label="song"
-                            name="song"
-                        // value={formInputs.song}
-                        // onChange={handleFormInputs}
-                        />
+        <Container className="center-align">
+            <h4 className="search__title">What's <span className="underline">your</span> favorite song?</h4>
+            <form className="search__container">
+                <TextInput
+                    icon="album"
+                    id="song"
+                    label="song"
+                    name="song"
+                    value={formInputs.song}
+                    onChange={handleFormInputs}
+                />
+                <TextInput
+                    icon="person"
+                    id="artist"
+                    label="artist"
+                    name="artist"
+                    value={formInputs.artist}
+                    onChange={handleFormInputs}
+                />
 
-                    </form>
-                </Col>
-            </Row>
+                <Button
+                    node="button"
+                    type="submit"
+                    waves="orange"
+                    onClick={handleSearch}
+                >
+                    Search
+                    <Icon right>send</Icon>
+                </Button>
+            </form>
         </Container>
     )
 }
 
-export default search;
+export default Search;
