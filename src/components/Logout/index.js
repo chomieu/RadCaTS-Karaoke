5c46e9a0585c8020246d761cbbd78a65cb0bc193
Tim Keller <timothy.m.keller@gmail.com>
2021-03-03 00:10:53 -0800

    className instead of class

--- src/components/Logout/index.js
+++ src/components/Logout/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Modal, Button } from 'react-materialize';
 import "./style.css";
 
-const trigger = <Button className="logout_button"><i class="material-icons">exit_to_app</i></Button>;
+const trigger = <Button className="logout_button"><i className="material-icons">exit_to_app</i></Button>;
 
 export default function logout({ userData, setUserData }) {
 
